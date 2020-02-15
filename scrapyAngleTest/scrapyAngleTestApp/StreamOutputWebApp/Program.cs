1890272e35ac69f477e27bbc34cf8813cd7efe1f
Dominik <32032778+dommyrock@users.noreply.github.com>
2020-02-15 17:52:09 +0100

    [wip] added web app  .net core 3 (react+redux template)

---
+++ scrapyAngleTest/scrapyAngleTestApp/StreamOutputWebApp/Program.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace StreamOutputWebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+           WebHost.CreateDefaultBuilder(args)
+               .UseStartup<Startup>();
+    }
+}
