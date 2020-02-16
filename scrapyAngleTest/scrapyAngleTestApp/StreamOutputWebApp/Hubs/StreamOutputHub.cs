29056009707925948db333ceede4bcfe5bdfdd82
Dominik <32032778+dommyrock@users.noreply.github.com>
2020-02-16 11:33:37 +0100

    [wip] made initial hub class,interface
    todo: impement streaming logic methods in "StreamOutput" class, & IStreamOutput

---
+++ scrapyAngleTest/scrapyAngleTestApp/StreamOutputWebApp/Hubs/StreamOutputHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using StreamOutputWebApp.Stream;
+
+namespace StreamOutputWebApp.Hubs
+{
+    public class StreamOutputHub : Hub
+    {
+        private readonly StreamOutput _streamOutput;
+
+        public StreamOutputHub(StreamOutput streamOutput)
+        {
+            _streamOutput = streamOutput;
+        }
+    }
+}
